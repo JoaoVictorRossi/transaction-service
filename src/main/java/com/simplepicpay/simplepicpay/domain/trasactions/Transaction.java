2fdc47bbd8cc04ccf53c7e6fee950322b46e742e
João Rossi <jvrossi204@gmail.com>
2023-10-15 20:32:47 -0300

    feat: create User and Transaction entities

---
+++ src/main/java/com/simplepicpay/simplepicpay/domain/trasactions/Transaction.java
@@ -0,0 +1,35 @@
+package com.simplepicpay.simplepicpay.domain.trasactions;
+
+import com.simplepicpay.simplepicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+    private LocalDateTime timestamp;
+
+}
---
+++ src/main/java/com/simplepicpay/simplepicpay/domain/user/UserType.java
@@ -0,0 +1,8 @@
+package com.simplepicpay.simplepicpay.domain.user;
+
+public enum UserType {
+
+    COMMUN,
+    MERCHANT;
+
+}
